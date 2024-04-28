ec50ac489eb3d6d337fddd54b1190645df47dbf0
David <david@Davids-MacBook-Pro-2.local>
2024-04-28 00:08:39 -0600

    Pivot.

--- ISA/ISA/urls.py
+++ ISA/ISA/urls.py
@@ -4,10 +4,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('eventcalendar.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('accounts.urls')),
-    path('profile/', include('user_profile.urls'))
+    path('profile/', include('user_profile.urls')),
+    path('golf_course/', include('golf_course_service.urls'))
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
--- ISA/user_profile/serializers.py
+++ ISA/user_profile/serializers.py
@@ -1,17 +1,7 @@
 from rest_framework import serializers
-from .models import UserProfile, Student, Instructor
+from .models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = '__all__'
-        
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:       
-        model = Student
-        fields = '__all__'
-
-class InstructorSerializer(serializers.ModelSerializer):
-    class Meta:       
-        model = Instructor
         fields = '__all__'
