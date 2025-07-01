e99c658681116a1fd6371b5a406a3c8168deb0aa
David <davidpollockcsu@gmail.com>
2025-06-30 22:07:07 -0700

    clean up csrf token service.

--- angular-frontend/src/app/csrf-token.service.ts
+++ angular-frontend/src/app/csrf-token.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CsrfTokenService {
 
-  private apiUrl = '//localhost:8000'
+  private readonly apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
-  
 
-  async fetchCsrfToken(): Promise<void> {
-
-    await this.http.get<any>(this.apiUrl + '/accounts/csrf_cookie', { observe: 'response', withCredentials: true })
-    .subscribe();
-
+  fetchCsrfToken(): Observable<HttpResponse<any>> {
+    return this.http.get<any>(`${this.apiUrl}/accounts/csrf_cookie`, {
+      observe: 'response',
+      withCredentials: true
+    });
   }
 }
