c5fa811f194af80a8a12cf0b884eaa9ddf55159b
David <davidpollockcsu@gmail.com>
2025-07-07 21:09:50 -0700

    packages

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { LayoutComponent } from './layout/layout.component';
-import { HttpClientModule, HttpClientXsrfModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi, withXsrfConfiguration } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,35 +12,21 @@ import { ScheduleViewComponent } from './dashboard/schedule-view/schedule-view.c
 import { DatePickerComponent } from './date-picker/date-picker.component';
 import { FormsModule } from '@angular/forms';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LayoutComponent,
-    NavbarComponent,
-    DashboardComponent,
-    ScheduleViewComponent,
-    DatePickerComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    HttpClientXsrfModule.withOptions({
-      cookieName: "csrftoken",
-      headerName: "X-CSRFToken",
-    }),
-    RouterModule.forRoot(
-      routeConfig),
-    // ... other imports
-  ],
-  bootstrap: [AppComponent],
-  providers: [
-    // [
-    //   { provide: HTTP_INTERCEPTORS, 
-    //     useClass: CsrfInterceptorService, 
-    //     multi: true }
-    // ],
-  ], // Add any app-wide providers here
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        LayoutComponent,
+        NavbarComponent,
+        DashboardComponent,
+        ScheduleViewComponent,
+        DatePickerComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(routeConfig)], providers: [
+        provideHttpClient(withInterceptorsFromDi(), withXsrfConfiguration({
+            cookieName: "csrftoken",
+            headerName: "X-CSRFToken",
+        }))
+    ] })
 export class AppModule {}
