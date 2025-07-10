d18fd5d646f5f2f95471e9ecdb6db3b34cb66fcb
David <davidpollockcsu@gmail.com>
2025-07-09 23:17:29 -0700

    dev container + csrf token.

--- angular-frontend/src/app/navbar/navbar.component.ts
+++ angular-frontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrl: './navbar.component.css',
-    standalone: false
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  standalone: false
 })
 export class NavbarComponent implements OnInit {
   currentUser$ = this.authService.currentUser$;
